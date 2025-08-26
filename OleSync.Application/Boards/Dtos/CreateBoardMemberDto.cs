848be16f41070b9d2632ce7fc7e30ff2c73ddf15
Reham Muhammad <My Lap@DESKTOP-KDGSK80>
2025-08-26 20:05:06 +0300

    Allow null for FullName, Email, Phone in CreateBoardMemberDto

--- OleSync.Application/Boards/Dtos/CreateBoardMemberDto.cs
+++ OleSync.Application/Boards/Dtos/CreateBoardMemberDto.cs
@@ -6,9 +6,9 @@ namespace OleSync.Application.Boards.Dtos
     {
         public int? EmployeeId { get; set; }
         public int? GuestId { get; set; }
-        public string FullName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string Phone { get; set; } = null!;
+        public string? FullName { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public string? Phone { get; set; } = null!;
         public string? Position { get; set; } = null!;
         public MemberType MemberType { get; set; }
         public MemberRole Role { get; set; }
